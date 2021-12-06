df2d25ee23f54ed90c52b3f466e60e4169012160
gauravraj01 <itsgauravraj01@gmail.com>
2021-12-06 11:54:47 +0530

    Upload success

---
+++ app.py
@@ -0,0 +1,16 @@
+from kubernetes import client, config
+
+config.load_kube_config()
+
+api = client.CustomObjectsApi()
+k8s_nodes = api.list_cluster_custom_object("metrics.k8s.io", "v1beta1", "nodes")
+k8s_pods = api.list_namespaced_custom_object("metrics.k8s.io", "v1beta1", "default", "pods")
+
+
+
+for stats in k8s_nodes['items']:
+    print("Node Name: %s\tCPU: %s\tMemory: %s" % (stats['metadata']['name'], stats['usage']['cpu'], stats['usage']['memory']))
+
+for stats in k8s_pods['items']:
+    print("Pod Name: %s\tCPU: %s\tMemory: %s" % (stats['metadata']['name'], stats['usage']['cpu'], stats['usage']['memory']))
+
