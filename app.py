86d1fde3743f90151370764e0244e0a8e23806a0
gauravraj01 <55096722+gauravraj01@users.noreply.github.com>
2021-12-06 12:08:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from kubernetes import client, config
 
 config.load_kube_config()
 
+print("Hello world")
+
 api = client.CustomObjectsApi()
 k8s_nodes = api.list_cluster_custom_object("metrics.k8s.io", "v1beta1", "nodes")
 k8s_pods = api.list_namespaced_custom_object("metrics.k8s.io", "v1beta1", "default", "pods")
